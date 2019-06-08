bea4bcdc8e727f4b7b3cb961bfea58b6b12024b0
AngelaG1 <agloria@ucdavis.edu>
2019-06-08 09:04:21 -0700

    initial commit

---
+++ Forms/Bind_DataGridView_Using_DataTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkGUI.Forms
+{
+    public partial class Bind_DataGridView_Using_DataTable : Form
+    {
+        public Bind_DataGridView_Using_DataTable()
+        {
+            InitializeComponent();
+        }
+
+        private void Bind_DataGridView_Using_DataTable_Load(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+
+            // add columns to datatable
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("First Name", typeof(string));
+            table.Columns.Add("Last Name", typeof(string));
+            table.Columns.Add("Age", typeof(int));
+
+            // add rows to datatable
+            table.Rows.Add(1, "First A", "Last A", 10);
+            table.Rows.Add(2, "First B", "Last B", 20);
+            table.Rows.Add(3, "First C", "Last C", 30);
+            table.Rows.Add(4, "First D", "Last D", 40);
+            table.Rows.Add(5, "First E", "Last E", 50);
+            table.Rows.Add(6, "First F", "Last F", 60);
+            table.Rows.Add(7, "First G", "Last G", 70);
+            table.Rows.Add(8, "First H", "Last H", 80);
+
+            dataGridView1.DataSource = table;
+
+        }
+    }
+}
---
+++ Forms/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkGUI
+{
+    public partial class Class1 : Form
+    {
+        public Class1()
+        {
+            InitializeComponent();
+        }
+
+        private void MultiplexImbalanceForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                firstorder = true;
+            else
+                firstorder = false;
+
+
+            nullModel = checkBox1.Checked;
+
+            isValid = true;
+            this.Close();
+        }
+        public bool isValid = false;
+        public bool firstorder = true;
+        public bool nullModel = false;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
